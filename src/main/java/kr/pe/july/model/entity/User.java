2afb76a9973b695414d302cdfcef851f0fe5c018
minichichi <minichi6985@gmail.com>
2022-07-18 14:52:51 +0900

    Update user

--- src/main/java/kr/pe/july/model/entity/User.java
+++ src/main/java/kr/pe/july/model/entity/User.java
@@ -1,5 +1,31 @@
 package kr.pe.july.model.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+
+@Entity
+public class User {
+	
+	@Id
+	@Column(name="user_name")
+	private String username;
+	@Column(name="pw")
+	private String password;
+	
+	private String role;
 }
