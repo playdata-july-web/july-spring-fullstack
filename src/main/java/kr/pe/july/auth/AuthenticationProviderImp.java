cd0b0e61639464ea549e0339cbe0f0ab55248313
jacob3015 <jacob3015@gmail.com>
2022-07-18 15:56:50 +0900

    Update authentication provider implementation

--- src/main/java/kr/pe/july/auth/AuthenticationProviderImp.java
+++ src/main/java/kr/pe/july/auth/AuthenticationProviderImp.java
@@ -1,5 +1,35 @@
 package kr.pe.july.auth;
 
-public class AuthenticationProviderImp {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+public class AuthenticationProviderImp implements AuthenticationProvider {
+	
+	@Autowired
+	private UserDetailsServiceImp service;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Override
+	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+		UserDetailsImp user = (UserDetailsImp) service.loadUserByUsername(authentication.getName());
+		String reqPassword = authentication.getCredentials().toString();
+		
+		if(!passwordEncoder.matches(reqPassword, user.getPassword())) {
+			throw new BadCredentialsException("Invalid Password");
+		}
+		
+		return new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+	}
+
+	@Override
+	public boolean supports(Class<?> authentication) {
+		return true;
+	}
 }
