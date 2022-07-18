fb235abb3736ba20651f63e4aa4a82089ce03386
jacob3015 <jacob3015@gmail.com>
2022-07-18 15:50:12 +0900

    Update user details service implementation

--- src/main/java/kr/pe/july/auth/UserDetailsServiceImp.java
+++ src/main/java/kr/pe/july/auth/UserDetailsServiceImp.java
@@ -1,5 +1,29 @@
 package kr.pe.july.auth;
 
-public class UserDetailsServiceImp {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import kr.pe.july.model.entity.User;
+import kr.pe.july.model.repository.UserRepository;
+
+public class UserDetailsServiceImp implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	private UserDetailsImp entityToDto(User user) {
+		ObjectMapper mapper = new ObjectMapper();
+		return mapper.convertValue(user, new TypeReference<UserDetailsImp>() {});
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return entityToDto(repository.findById(username).orElseThrow(() -> new UsernameNotFoundException("User Not Found : " + username)));
+	}
+	
 }
