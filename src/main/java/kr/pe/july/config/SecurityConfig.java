87a1ce069c29d60c08bd4bbab5cd306d43e71b32
jacob3015 <jacob3015@gmail.com>
2022-07-18 16:16:55 +0900

    Update security configuration

--- src/main/java/kr/pe/july/config/SecurityConfig.java
+++ src/main/java/kr/pe/july/config/SecurityConfig.java
@@ -1,5 +1,57 @@
 package kr.pe.july.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+import kr.pe.july.auth.AuthenticationProviderImp;
+import kr.pe.july.auth.UserDetailsServiceImp;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	private final String API = "api";
+	
+	@Bean
+	public AuthenticationProviderImp getProvider() {
+		return new AuthenticationProviderImp();
+	}
+	
+	@Bean
+	public UserDetailsServiceImp getService() {
+		return new UserDetailsServiceImp();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder getPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		
+		http.authorizeRequests()
+			.antMatchers(API + "/admin").hasRole("ADMIN")
+			.antMatchers(API+ "/member").hasRole("MEMBER")
+			.anyRequest().permitAll();
+		
+		http.formLogin()
+			.loginProcessingUrl(API + "/login");
+		
+		http.logout()
+			.logoutUrl(API + "logout");
+		
+		http.sessionManagement()
+			.maximumSessions(1)
+			.maxSessionsPreventsLogin(true);
+		
+		http.authenticationProvider(getProvider());
+		
+		return http.build();
+	}
 }
