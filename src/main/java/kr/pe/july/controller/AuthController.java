ad60b7cb97277eecd57f0b8f754d689b5eb22a54
jacob3015 <jacob3015@gmail.com>
2022-07-21 09:10:35 +0900

    Update connection login and signup

--- src/main/java/kr/pe/july/controller/AuthController.java
+++ src/main/java/kr/pe/july/controller/AuthController.java
@@ -1,34 +1,28 @@
 package kr.pe.july.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
-@RestController
+@Controller
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
 	
-	@GetMapping("/admin")
-	public String admin() {
-		return "ADMIN 인증 성공";
+	@GetMapping("/form")
+	public String form() {
+		return "login_form";
 	}
 	
-	@GetMapping("/member")
-	public String member() {
-		return "MEMBER 인증 성공";
+	@GetMapping("/login")
+	public String loginSuccess() {
+		return "login_form";
 	}
 	
-	@PostMapping("/fail")
-	public String fail() {
-		return "로그인 실패";
-	}
-	
-	@GetMapping("/home")
-	public String home() {
-		return "로그아웃 성공";
+	@GetMapping("/logout")
+	public String logout() {
+		return "login_form";
 	}
 }
